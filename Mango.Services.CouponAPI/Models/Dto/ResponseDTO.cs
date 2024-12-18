578764d40a2fa9ae5fceb4e76c33fc0b55278a03
keksikls <151632212+keksikls@users.noreply.github.com>
2024-12-18 22:27:28 +0300

    AddDbContext-AddHttpGet-AddHttpGetAll

---
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class ResponseDTO
+    {
+        public Object? Result { get; set; }
+        public bool IsSucces { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
